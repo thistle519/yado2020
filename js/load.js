333289078e95d6a5d11a0b373ffbd767fd1118ee
hotsumi <45584045+Hotsukai@users.noreply.github.com>
2020-03-27 11:27:13 +0900

    Revert "ジャンボトロンをJSで書くようにした。"
    This reverts commit 2aaa109af0c76c6f1b81ba086246a473b60ce9b1.

--- js/load.js
+++ js/load.js
@@ -9,28 +9,6 @@ function writeHeader() {
     }
   });
 }
-function writeJumbotron(page_title, picture_name) {
-  const default_pic_name = "jumbotron_test2.jpg";
-  if(page_title==undefined){
-    return;
-  }
-  if (picture_name == undefined) {
-    picture_name = default_pic_name;
-  }
-  $.ajax({
-    url: "./js/include/jumbotron.html",
-    cache: false,
-    async: false,
-    success: function(jumbotron) {
-      
-      var jumbotron = jumbotron.replace("PAGE_TITLE", page_title);
-      var jumbotron = jumbotron.replace("PICTURE_NAME", picture_name);
-
-      document.write(jumbotron);
-      console.log(jumbotron);
-    }
-  });
-}
 
 function writeWebAd() {
   $.ajax({
